bd71a014529aac97a57d2e0b9f1d1c90ba6e0bb6
Peter Zimmermann <pzimm30@gmail.com>
2022-12-22 14:04:12 -0500

    update the default.nix after further playing with chatgpt

--- default.nix
+++ default.nix
@@ -3,12 +3,13 @@
 pkgs.stdenv.mkDerivation {
   name = "textedit";
   buildInputs = [ pkgs.gcc pkgs.sbcl ];
-  src = ./.;
+  src = ./src;
   phases = "installPhase";
   installPhase = ''
     mkdir -p $out/bin
     gcc src/textedit.c -o $out/bin/textedit
     cp src/textedit.lisp $out/bin/textedit.lisp
+    sbcl --script $out/bin/textedit.lisp
   '';
 }
 
