85c33ed7aa9db25a7185bddeb9e83bb2fbdfebfc
Peter Zimmermann <pzimm30@gmail.com>
2022-12-22 14:00:18 -0500

    had chatgpt make this for me, will fix it if there are any bugs later on

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-let
-  pkgs = import <nixpkgs> {};
-in
+{ pkgs ? import <nixpkgs> {} }:
+
 pkgs.stdenv.mkDerivation {
-  name = "my-env";
-
-  buildInputs =
-    [
-
-    ];
+  name = "textedit";
+  buildInputs = [ pkgs.gcc pkgs.sbcl ];
+  src = ./.;
+  phases = "installPhase";
+  installPhase = ''
+    mkdir -p $out/bin
+    gcc src/textedit.c -o $out/bin/textedit
+    cp src/textedit.lisp $out/bin/textedit.lisp
+  '';
 }
+
+
